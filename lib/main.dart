c3b25d8a7b08d4c950abd11b97e366f526a43d82
Ryan Kristoffer <77266096+bot-ryan@users.noreply.github.com>
2025-11-05 17:18:29 +0800

    simplified main.dart after adding the app_router.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
+import 'router/app_router.dart'; //to do routing such as /login and /home
 import 'package:flutter_web_plugins/url_strategy.dart'; 
 
 
-//web pages
-import 'pages/login_signup/login_signup_page.dart';
-import 'pages/home/home_page.dart';
+
 
 
 
@@ -17,37 +15,17 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // 1) Create the router
-  static final _router = GoRouter(
-    // Which URL opens first if none specified:
-    initialLocation: '/login',
-
-    // 2) Declare routes
-    routes: [
-      GoRoute(
-        path: '/home',
-        name: 'home',
-        builder: (context, state) => const HomePage(),
-      ),
-      GoRoute(
-        path: '/login',
-        name: 'login',
-        builder: (context, state) => const LoginSignupPage(),
-      ),
-    ],
-  );
-
   @override
   Widget build(BuildContext context) {
     // 3) Use MaterialApp.router
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       title: 'Realfun Attendance',
-      routerConfig: _router, 
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      routerConfig: AppRouter.router,
     );
   }
 }
